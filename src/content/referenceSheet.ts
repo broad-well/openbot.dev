725f7c8b81bb688cae0a388acd260233d6732a5a
Michael Peng <michael@mcmoo.org>
2023-01-03 01:58:02 -0500

    Reach first deployable completion

--- src/content/referenceSheet.ts
+++ src/content/referenceSheet.ts
@@ -1,18 +1,19 @@
 export default {
-    input: {
-        title: 'Input',
-        entries: {
-            when: {
-                title: 'Event handler',
-                subtitle: 'Whenever this happens, do that',
-                code: `mod.when(Events.MessageCreate, event => await event.react('✨'))
+  input: {
+    title: "Input",
+    entries: {
+      when: {
+        title: "Event handler",
+        subtitle: "Whenever this happens, do that",
+        code: `mod.when(Events.MessageCreate, event => await event.react('✨'))
 // When this happens, do that once and never again:
-mod.once(Events.ClientReady, event => console.log('module started!'))`
-            },
-            command: {
-                title: 'Slash commands',
-                subtitle: 'Build a chat input command and validate/fulfill interactions',
-                code: `mod.command('ping', 'Get a response from the bot',
+mod.once(Events.ClientReady, event => console.log('module started!'))`,
+      },
+      command: {
+        title: "Slash commands",
+        subtitle:
+          "Build a chat input command and validate/fulfill interactions",
+        code: `mod.command('ping', 'Get a response from the bot',
             intx => await intx.reply('pong!'))
 // Fully customized:
 mod.command('quote', 'Short description', {
@@ -34,12 +35,13 @@ mod.command('quote', 'Short description', {
   },
   /* optional: limit this command to a given guild */
   guild: '1002274815270465607'
-})`
-            },
-            menu: {
-                title: 'Context menu items',
-                subtitle: 'Attach a new context menu button to a message or a user, and do this when someone clicks it',
-                code: `mod.menu('Menu item label', ApplicationCommandType.User, intx => { /* what to do? */ })
+})`,
+      },
+      menu: {
+        title: "Context menu items",
+        subtitle:
+          "Attach a new context menu button to a message or a user, and do this when someone clicks it",
+        code: `mod.menu('Menu item label', ApplicationCommandType.User, intx => { /* what to do? */ })
 
 mod.menu('Menu item label', ApplicationCommandType.Message, {
   build(builder) {
@@ -58,20 +60,117 @@ mod.menu('Menu item label', ApplicationCommandType.Message, {
   },
   /* optional: limit this command to a given guild */
   guild: '12345678901234567890'
-})`
-            }
+})`,
+      },
+    },
+  },
+  terms: {
+    title: "Terms",
+    entries: {
+      snowflake: {
+        title: '❄️ Snowflake',
+        subtitle: 'Unique ID that represents an entity (message, user, channel, guild, etc.) on Discord. Example: 135824500603224064'
+      },
+      guild: {
+        title: 'Guild',
+        subtitle: 'Discord server (technical term for disambiguation)'
+      },
+      memberUser: {
+        title: 'Member vs User',
+        subtitle: "A User is a human's Discord account (e.g. username#ID). A Member is a User's profile in a Guild (server profile, roles, etc.)"
+      },
+      command: {
+        title: 'Command',
+        subtitle: 'Interactive feature provided by a bot. Types: Slash command, context menu command',
+        links: {
+          Docs: 'https://discord.com/developers/docs/interactions/application-commands#application-commands'
         }
-    },
-    terms: {
-        title: 'Terms',
-
-    },
-    queries: {
-        title: 'Queries',
-
-    },
-    output: {
-        title: 'Output',
-
+      },
+      intent: {
+        title: 'Gateway Intent',
+        subtitle: 'Group of events that take place on Discord. Specify intents to receive their events from Discord',
+        links: {
+          List: 'https://discord.com/developers/docs/topics/gateway#list-of-intents'
+        }
+      }
     }
-}
+  },
+  queries: {
+    title: "Queries",
+  },
+  output: {
+    title: "Output",
+    entries: {
+      textReply: {
+        title: "Plain-text reply",
+        subtitle: "Reply to the user that invoked the slash command",
+        code: `async (intx: ChatInputCommandInteraction) => {
+  // everyone in the guild can see:
+  await intx.reply('sus?')
+  // only the user can see:
+  await intx.reply({
+    content: 'sus?',
+    ephemeral: true
+  })
+  // DM the user
+  await intx.user.send('sus?')
+}`
+      },
+      reactions: {
+        title: 'Reactions',
+        subtitle: 'React to any given message (e.g. from MessageCreate)',
+        code: `async (msg: MessageEvent) => {
+  await msg.react('😎')
+}`
+      },
+      richReply: {
+        title: "Embeds and buttons",
+        subtitle: "Display embedded cards and buttons for more interaction",
+        links: {
+          'Embed Guide': 'https://discordjs.guide/popular-topics/embeds.html#using-an-embed-object',
+          'Embed Preview': 'https://discohook.org/'
+        },
+        code: `async (intx: ChatInputCommandInteraction) => {
+  // Display a button "✅ Agree" under the message
+  const buttons = new ActionRowBuilder<ButtonBuilder>({
+    components: [{
+      type: ComponentType.Button,
+      customId: 'custom-agree',
+      label: 'Agree',
+      emoji: '✅',
+      style: ButtonStyle.Primary
+    }]
+  })
+  await intx.reply({
+    // Build and preview embeds at https://discohook.org
+    embeds: [{
+      title: 'Some title',
+      description: "Embeds are cool because humans can't send them legally"
+    }],
+    components: [buttons]
+  })
+}`
+      },
+      modals: {
+        title: 'Modals',
+        subtitle: 'Display a form in a focused dialog to the user',
+        links: {
+          'Modal Guide': 'https://discordjs.guide/interactions/modals.html#building-and-responding-with-modals'
+        },
+        code: `async (intx: CommandInteraction) => {
+  const modal = new ModalBuilder()
+    .setCustomId('form-modal')
+    .setTitle('My Modal')
+  const question = new TextInputBuilder()
+    .setCustomId('number-input')
+    .setLabel('Enter any significant number')
+    .setStyle(TextInputStyle.Short)
+  const firstRow = new ActionRowBuilder<ModalActionRowComponentBuilder>()
+    .addComponents(question)
+  modal.addComponents(firstRow)
+  await intx.showModal(modal)
+}`
+      }
+    }
+  },
+};
