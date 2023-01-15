870b77311f48c804322906714fe1fb5e4822a96c
Michael Peng <michael@mcmoo.org>
2023-01-15 14:28:46 -0500

    Rearrange shortcuts and add 2 more

--- src/content/shortcuts.ts
+++ src/content/shortcuts.ts
@@ -9,6 +9,11 @@ export default {
         description: 'Where OpenBot collaboration and version control officially takes place',
         target: 'https://gitlab.umich.edu/living-artsengineers/open-bot'
     },
+    slides: {
+        label: 'Slide deck',
+        description: 'Presentation slides for the OpenBot Workshop on January 15th',
+        target: 'https://pitch.com/public/9010e62a-5f09-4052-b05c-7e3b6a756664'
+    },
     Typedoc: {
         label: 'Cordette TypeDoc',
         description: 'Detailed documentation of the classes and methods provided by the module library',
@@ -18,5 +23,10 @@ export default {
         label: 'Discord.js Guide',
         description: 'Guide to using Discord.js, the underlying library supporting all interactions with Discord',
         target: 'https://discordjs.guide'
+    },
+    discordDev: {
+        label: 'Discord Dev Portal',
+        description: 'Documentation of bot capabilities and technical concepts',
+        target: 'https://discord.com/developers/docs/intro'
     }
 }
